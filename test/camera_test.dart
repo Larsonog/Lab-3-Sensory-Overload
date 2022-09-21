97c5bca02d20d98bede9544b14928e007fed0eb1
Alexandria <alexandriacade0@gmail.com>
2022-09-20 22:30:01 -0500

    Camera unit tests
    Camera unit tests created. Failing because of availableCameras(). Is future

--- test/camera_test.dart
+++ test/camera_test.dart
@@ -10,35 +10,42 @@ class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
 void main() {
   testWidgets('Take Picture Screen has image displayed', (tester) async {
-    final cameras = await availableCameras();
-    final firstCamera = cameras.first;
-    await tester
-        .pumpWidget(MaterialApp(home: TakePictureScreen(camera: firstCamera)));
+    await tester.runAsync(() async {
+      final cameras = await availableCameras();
+      final firstCamera = cameras.first;
+      await tester.pumpWidget(
+          MaterialApp(home: TakePictureScreen(camera: firstCamera)));
 
-    expect(find.byType(CameraPreview), findsOneWidget);
+      expect(find.byType(CameraPreview), findsOneWidget);
+    });
   });
 
   testWidgets('Take picture button appears on screen', (tester) async {
-    final cameras = await availableCameras();
-    final firstCamera = cameras.first;
-    await tester.pumpWidget(MaterialApp(home:TakePictureScreen(camera: firstCamera)));
-    final takePictureButton = find.byType(FloatingActionButton);
-    expect(takePictureButton, findsOneWidget);
+    await tester.runAsync(() async {
+      final cameras = await availableCameras();
+      final firstCamera = cameras.first;
+      await tester.pumpWidget(
+          MaterialApp(home: TakePictureScreen(camera: firstCamera)));
+      final takePictureButton = find.byType(FloatingActionButton);
+      expect(takePictureButton, findsOneWidget);
+    });
   });
 
 //https://stackoverflow.com/questions/50704647/how-to-test-navigation-via-navigator-in-flutter
   testWidgets('Take picture button goes to journal entry screen',
       (WidgetTester tester) async {
-    final cameras = await availableCameras();
-    final firstCamera = cameras.first;
-    final mockObserver = MockNavigatorObserver();
-    await tester.pumpWidget(TakePictureScreen(camera: firstCamera));
+    await tester.runAsync(() async {
+      final cameras = await availableCameras();
+      final firstCamera = cameras.first;
+      final mockObserver = MockNavigatorObserver();
+      await tester.pumpWidget(TakePictureScreen(camera: firstCamera));
 
-    final takePictureButton = find.byType(FloatingActionButton);
-    expect(takePictureButton, findsOneWidget);
-    await tester.tap(takePictureButton);
-    await tester.pumpAndSettle();
+      final takePictureButton = find.byType(FloatingActionButton);
+      expect(takePictureButton, findsOneWidget);
+      await tester.tap(takePictureButton);
+      await tester.pumpAndSettle();
 
-    expect(find.byType(JournalEntryScreen), findsOneWidget);
+      expect(find.byType(JournalEntryScreen), findsOneWidget);
+    });
   });
 }
